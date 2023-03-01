d4249125b527ab6a36dbda195e6beed1252b847b
morjep <94778792+morjep@users.noreply.github.com>
2023-03-01 20:28:15 +0100

    cleaned up layout utils

--- app/components/Flow/generateLayoutFromData.tsx
+++ app/components/Flow/generateLayoutFromData.tsx
@@ -3,6 +3,49 @@ import type { Node, Edge } from "reactflow";
 
 const splitLines = (str: string) => str.split(/\r?\n/);
 
+const countParentNodes = (lines: string[]): number => {
+  const level2Nodes = lines.filter((line: string) => line.trim().startsWith("** ")).length;
+  return level2Nodes;
+};
+
+const extractLevel = (line: string): number => {
+  const level = (line.match(/\*/g) || []).length;
+  return level;
+};
+
+const extractContent = (line: string): string => {
+  const text = line.replace(/\*/g, "");
+  return text;
+};
+
+const extractDueDate = (line: string): string => {
+  const dueDate = line.match(/__due_.*__/g);
+  const dueDateStr = dueDate ? dueDate[0].replace(/__due_/g, "").replace(/__/g, "") : "";
+  return dueDateStr;
+};
+
+const extractIndicator = (line: string): string => {
+  const indicator = line.match(/__indicator_.*__/g);
+  let indicatorStr = indicator ? indicator[0].replace(/__indicator_/g, "").replace(/__/g, "") : "";
+
+  indicatorStr = line.match(/__is__/g) ? "success" : indicatorStr;
+  indicatorStr = line.match(/__iw__/g) ? "warning" : indicatorStr;
+  indicatorStr = line.match(/__ie__/g) ? "error" : indicatorStr;
+  return indicatorStr;
+};
+
+const extractProgressBar = (line: string): boolean => {
+  const progress = line.match(/__pb.*__/g);
+  const progressBar = progress ? true : false;
+  return progressBar;
+};
+
+const extractProgressValue = (line: string): string => {
+  const progress = line.match(/__pb.*__/g);
+  const progressValue = progress ? progress[0].replace(/__pb/g, "").replace(/__/g, "") : "0";
+  return progressValue;
+};
+
 const generateLayoutFromData = (data: String) => {
   const nodes: Node[] = [];
   const edges: Edge[] = [];
@@ -18,48 +61,25 @@ const generateLayoutFromData = (data: String) => {
   let edgeSource = "";
   let edgeTarget = "";
 
-  // First split the data into lines
   const lines = splitLines(data as string);
-
-  // Count the number of level 2 nodes
-  const level2Nodes = lines.filter((line: string) => line.trim().startsWith("** ")).length;
-  console.log("Level 2 nodes:", level2Nodes);
+  const level2Nodes = countParentNodes(lines);
 
   lines.forEach((line: string) => {
     id = id + 1;
 
-    // Count the number of # in the line
-    const level = (line.match(/\*/g) || []).length;
-    // Remove the # from the line
-    const label = line.replace(/\*/g, "");
-
-    // due date is indicated by __due__ in the line, so we need to extract that
-    const dueDate = label.match(/__due_.*__/g);
-    const dueDateStr = dueDate ? dueDate[0].replace(/__due_/g, "").replace(/__/g, "") : "";
-
-    // Indicator is indicated by __indicator__ in the line, so we need to extract that
-    // Indicator can be success, warning or error
-    // Indicator also has a short form: __is__, __iw__ and __ie__
-    const indicator = label.match(/__indicator_.*__/g);
-    let indicatorStr = indicator
-      ? indicator[0].replace(/__indicator_/g, "").replace(/__/g, "")
-      : "";
-
-    indicatorStr = label.match(/__is__/g) ? "success" : indicatorStr;
-    indicatorStr = label.match(/__iw__/g) ? "warning" : indicatorStr;
-    indicatorStr = label.match(/__ie__/g) ? "error" : indicatorStr;
-
-    // Progress bar is indicated by __pbXX__ in the line, so we need to extract that
-    const progress = label.match(/__pb.*__/g);
-    const progressBar = progress ? true : false;
-    const progressValue = progress ? progress[0].replace(/__pb/g, "").replace(/__/g, "") : "0";
+    const level = extractLevel(line);
+    const content = extractContent(line);
+    const dueDate = extractDueDate(content);
+    const indicator = extractIndicator(content);
+    const progressBar = extractProgressBar(content);
+    const progressValue = extractProgressValue(content);
 
     // remove the meta data from the label (should not be any left)
-    let labelStr = label.replace(/__.*__/g, "");
+    let label = content.replace(/__.*__/g, "");
 
     // Shorten the label if it is too long
-    if (labelStr.length > 50) {
-      labelStr = labelStr.substring(0, 50) + "...";
+    if (label.length > 50) {
+      label = label.substring(0, 50) + "...";
     }
 
     if (level != 0) {
@@ -82,7 +102,7 @@ const generateLayoutFromData = (data: String) => {
       if (level === 3) {
         nodePosition = { x: parentX + 35, y: childYoffset + 100 };
         nodeType = "child";
-        if (labelStr.length < 26) {
+        if (label.length < 26) {
           childYoffset = childYoffset + 60; // for the next child
         } else {
           childYoffset = childYoffset + 80; // for the next child
@@ -90,26 +110,20 @@ const generateLayoutFromData = (data: String) => {
         edgeSource = parentId.toString();
         edgeTarget = id.toString();
       }
-      // Shorten the label if it is too long
-      if (labelStr.length > 50) {
-        labelStr = labelStr.substring(0, 50) + "...";
-      }
 
       // Create the node
       const node: Node = {
         id: id.toString(),
         data: {
-          label: labelStr.trim(),
-          due: dueDateStr,
-          indicator: indicatorStr,
+          label: label.trim(),
+          due: dueDate,
+          indicator: indicator,
           progressBar: progressBar,
           progressValue: progressValue,
         },
         position: nodePosition,
-        // extent: nodeExtent,
         type: nodeType,
       };
-      // console.log("Node:", node);
       nodes.push(node);
 
       // Create the edge
@@ -129,7 +143,6 @@ const generateLayoutFromData = (data: String) => {
           stroke: "#a3a3a3",
         },
       };
-
       edges.push(edge);
     }
   });
