63fe2090586aae5b5391d87687f9dcc48d840577
morjep <94778792+morjep@users.noreply.github.com>
2023-03-10 09:17:47 +0100

    Refactor layout generator, support for comments

--- app/components/Flow/generateLayoutFromData.tsx
+++ app/components/Flow/generateLayoutFromData.tsx
@@ -1,50 +1,6 @@
 import { MarkerType } from "reactflow";
 import type { Node, Edge } from "reactflow";
-
-const splitLines = (str: string) => str.split(/\r?\n/);
-
-const countParentNodes = (lines: string[]): number => {
-  const level2Nodes = lines.filter((line: string) => line.trim().startsWith("** ")).length;
-  return level2Nodes;
-};
-
-const extractLevel = (line: string): number => {
-  const level = (line.match(/\*/g) || []).length;
-  return level;
-};
-
-const extractContent = (line: string): string => {
-  const text = line.replace(/\*/g, "");
-  return text;
-};
-
-const extractDueDate = (line: string): string => {
-  const dueDate = line.match(/__due_.*__/g);
-  const dueDateStr = dueDate ? dueDate[0].replace(/__due_/g, "").replace(/__/g, "") : "";
-  return dueDateStr;
-};
-
-const extractIndicator = (line: string): string => {
-  const indicator = line.match(/__indicator_.*__/g);
-  let indicatorStr = indicator ? indicator[0].replace(/__indicator_/g, "").replace(/__/g, "") : "";
-
-  indicatorStr = line.match(/__is__/g) ? "success" : indicatorStr;
-  indicatorStr = line.match(/__iw__/g) ? "warning" : indicatorStr;
-  indicatorStr = line.match(/__ie__/g) ? "error" : indicatorStr;
-  return indicatorStr;
-};
-
-const extractProgressBar = (line: string): boolean => {
-  const progress = line.match(/__pb.*__/g);
-  const progressBar = progress ? true : false;
-  return progressBar;
-};
-
-const extractProgressValue = (line: string): string => {
-  const progress = line.match(/__pb.*__/g);
-  const progressValue = progress ? progress[0].replace(/__pb/g, "").replace(/__/g, "") : "0";
-  return progressValue;
-};
+import invariant from "tiny-invariant";
 
 const generateLayoutFromData = (data: String) => {
   const nodes: Node[] = [];
@@ -64,114 +20,95 @@ const generateLayoutFromData = (data: String) => {
   let edgePrev = "";
   let connectParents = false;
 
-  const lines = splitLines(data as string);
-  const level2Nodes = countParentNodes(lines);
+  const wbsLines = getWbsLines(data);
+  const level2Nodes = getLevel2Nodes(wbsLines);
 
-  lines.forEach((line: string) => {
-    id = id + 1;
+  if (wbsLines) {
+    wbsLines.forEach((line: string) => {
+      id = id + 1;
 
-    const level = extractLevel(line);
-    const content = extractContent(line);
+      const { level, content, comment } = getLevelAndContent(line);
 
-    const dueDate = extractDueDate(content);
-    const indicator = extractIndicator(content);
-    const progressBar = extractProgressBar(content);
-    const progressValue = extractProgressValue(content);
+      const dueDate = extractDueDate(content);
+      const indicator = extractIndicator(content);
+      const progressBar = extractProgressBar(content);
+      const progressValue = extractProgressValue(content);
 
-    // Only check if we are on the first level (i.e. top level) - remember for subsequent passes
-    if (level === 1 && connectParents === false) {
-      connectParents = content.match(/__connect_parents__/g) ? true : false;
-    }
+      // Only check if we are on the first level (i.e. top level) - remember for subsequent passes
+      if (level === 1 && connectParents === false) {
+        connectParents = content.match(/__connect_parents__/g) ? true : false;
+      }
 
-    // remove the meta data from the label (should not be any left)
-    let label = content.replace(/__.*__/g, "");
+      // remove the meta data from the label (should not be any left)
+      let label = content.replace(/__.*__/g, "");
 
-    // Shorten the label if it is too long
-    if (label.length > 50) {
-      label = label.substring(0, 50) + "...";
-    }
+      // Shorten the label if it is too long
+      if (label.length > 50) {
+        label = label.substring(0, 50) + "...";
+      }
 
-    if (level != 0) {
-      if (level === 1) {
-        topId = id;
-        // Drop the top node edges if we are connecting parents
-        if (connectParents) {
-          topId = -1;
+      if (level != 0 && level < 4) {
+        if (level === 1) {
+          topId = id;
+          // Drop the top node edges if we are connecting parents
+          if (connectParents) {
+            topId = -1;
+          }
+          nodePosition = { x: 0, y: 0 };
+          nodeType = "top";
         }
-        nodePosition = { x: 0, y: 0 };
-        nodeType = "top";
-      }
-      if (level === 2) {
-        parentId = id;
-        parentCount = parentCount + 1;
-        parentX =
-          parentCount * parentXoffset - (level2Nodes * parentXoffset) / 2 - parentXoffset / 2;
-        childYoffset = 100;
-        nodePosition = { x: parentX, y: 100 };
-        nodeType = "parent";
-        edgeSource = topId.toString();
-        edgeTarget = id.toString();
+        if (level === 2) {
+          parentId = id;
+          parentCount = parentCount + 1;
+          parentX =
+            parentCount * parentXoffset - (level2Nodes * parentXoffset) / 2 - parentXoffset / 2;
+          childYoffset = 100;
+          nodePosition = { x: parentX, y: 100 };
+          nodeType = "parent";
+          edgeSource = topId.toString();
+          edgeTarget = id.toString();
 
-        edgePrev = prevParentId;
-        prevParentId = id.toString();
-      }
-      if (level === 3) {
-        nodePosition = { x: parentX + 35, y: childYoffset + 100 };
-        nodeType = "child";
-        if (label.length < 26) {
-          childYoffset = childYoffset + 60; // for the next child
-        } else {
-          childYoffset = childYoffset + 80; // for the next child
+          edgePrev = prevParentId;
+          prevParentId = id.toString();
         }
-        edgeSource = parentId.toString();
-        edgeTarget = id.toString();
-      }
+        if (level === 3) {
+          nodePosition = { x: parentX + 35, y: childYoffset + 100 };
+          nodeType = "child";
+          if (label.length < 26) {
+            childYoffset = childYoffset + 60; // for the next child
+          } else {
+            childYoffset = childYoffset + 80; // for the next child
+          }
+          if (progressBar) {
+            childYoffset = childYoffset + 30; // for the next child
+          }
+          edgeSource = parentId.toString();
+          edgeTarget = id.toString();
+        }
 
-      // Create the node
-      const node: Node = {
-        id: id.toString(),
-        data: {
-          label: label.trim(),
-          due: dueDate,
-          indicator: indicator,
-          progressBar: progressBar,
-          progressValue: progressValue,
-        },
-        position: nodePosition,
-        type: nodeType,
-      };
-      nodes.push(node);
+        // Create the node
+        const node: Node = {
+          id: id.toString(),
+          data: {
+            label: label.trim(),
+            due: dueDate,
+            indicator: indicator,
+            progressBar: progressBar,
+            progressValue: progressValue,
+          },
+          position: nodePosition,
+          type: nodeType,
+        };
+        nodes.push(node);
 
-      // Create the edge
-      const edge: Edge = {
-        id: id.toString(),
-        source: edgeSource,
-        sourceHandle: "source",
-        target: edgeTarget,
-        targetHandle: "target",
-        type: "smoothstep",
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-          width: 20,
-          height: 20,
-          color: "#a3a3a3",
-        },
-        style: {
-          strokeWidth: 1,
-          stroke: "#a3a3a3",
-        },
-      };
-      edges.push(edge);
-
-      // Create the edge to the previous parent
-      if (connectParents && edgePrev != "") {
+        // Create the edge
         const edge: Edge = {
           id: id.toString(),
-          source: edgePrev,
-          sourceHandle: "prev",
+          source: edgeSource,
+          sourceHandle: "source",
           target: edgeTarget,
-          targetHandle: "next",
-          type: "bezier",
+          targetHandle: "target",
+          type: "smoothstep",
           markerEnd: {
             type: MarkerType.ArrowClosed,
             width: 20,
@@ -179,16 +116,98 @@ const generateLayoutFromData = (data: String) => {
             color: "#a3a3a3",
           },
           style: {
-            strokeWidth: 2,
+            strokeWidth: 1,
             stroke: "#a3a3a3",
           },
         };
         edges.push(edge);
+
+        // Create the edge to the previous parent
+        if (connectParents && edgePrev != "") {
+          const edge: Edge = {
+            id: id.toString(),
+            source: edgePrev,
+            sourceHandle: "prev",
+            target: edgeTarget,
+            targetHandle: "next",
+            type: "bezier",
+            markerEnd: {
+              type: MarkerType.ArrowClosed,
+              width: 20,
+              height: 20,
+              color: "#a3a3a3",
+            },
+            style: {
+              strokeWidth: 2,
+              stroke: "#a3a3a3",
+            },
+          };
+          edges.push(edge);
+        }
       }
-    }
-  });
+    });
+  }
 
   return { nodes, edges };
 };
 
 export default generateLayoutFromData;
+
+// Utility functions below here - hoisted to the top of the file
+
+function getWbsLines(data: String) {
+  const regex = /@startwbs([\s\S]*?)@endwbs/g;
+  const match = regex.exec(data);
+
+  if (match) {
+    const wbsLines = match[1].trim().split("\n");
+    return wbsLines;
+  } else {
+    return [];
+  }
+}
+
+function getLevel2Nodes(lines: string[]): number {
+  const level2Nodes = lines.filter((line: string) => line.trim().startsWith("** ")).length;
+  return level2Nodes;
+}
+
+function getLevelAndContent(line: string) {
+  const regex = /^(?<level>\*+)\s*(?<text>.+?)(?<comment>\s*\/\/.*)?$/gm;
+  const found = regex.exec(line.trimStart());
+  const level = found?.groups?.level?.length ?? 0;
+  const text = found?.groups?.text ?? "";
+  const comment = found?.groups?.comment?.trim() ?? "";
+  console.log(`Level ${level}: ${text} ${comment}`);
+  return { level, content: text, comment };
+}
+
+function extractDueDate(line: string): string {
+  const dueDateRegex = /__due_(?<date>.*)__/g;
+  const match = dueDateRegex.exec(line);
+  const dueDateStr = match?.groups?.date ?? "";
+  return dueDateStr;
+}
+
+function extractIndicator(line: string): string {
+  const indicator = line.match(/__indicator_.*__/g);
+  let indicatorStr = indicator ? indicator[0].replace(/__indicator_/g, "").replace(/__/g, "") : "";
+
+  indicatorStr = line.match(/__is__/g) ? "success" : indicatorStr;
+  indicatorStr = line.match(/__iw__/g) ? "warning" : indicatorStr;
+  indicatorStr = line.match(/__ie__/g) ? "error" : indicatorStr;
+  return indicatorStr;
+}
+
+function extractProgressBar(line: string): boolean {
+  const progress = line.match(/__pb.*__/g);
+  const progressBar = progress ? true : false;
+  return progressBar;
+}
+
+function extractProgressValue(line: string): string {
+  const progressRegex = /__pb(?<value>\d+)__/g;
+  const match = progressRegex.exec(line);
+  const progressValue = match?.groups?.value ?? "0";
+  return progressValue;
+}
--- app/models/ai.server.ts
+++ app/models/ai.server.ts
@@ -19,10 +19,22 @@ const chatCompletion = async (currentContent: string, userRequest: string) => {
     * Top level task or item
     ** Second level task or item, also known as the parent
     *** Third level task or item, also known as the child
+    @endWBS // End of WBS
+
+    The wbs content must always be between the @startWBS and @endWBS tags.
+
     Rule 1: The number of asterisks indicates the level of the task or item.
     Rule 2: It is forbidden to use four or more asterisks.
     Rule 3: It must be asterisk and no other character to indicate the level of the task or item.
-    @endWBS // End of WBS
+    Rule 4: Use // to add a comment to the individual task or item, i.e. ** Second level task or item // This is a comment
+
+    There are a number of tags that can be used to indicate the status of the task or item:
+    __pb<value>__ = progress bar, where <value> is the percentage of completion, example: __pb50__ means 50% complete
+    __is__ = indicator_success
+    __iw__ = indicator_warning
+    __ie__ = indicator_error
+    __due_<value>__ = due date, where <value> is the duedate in any string format, i.e. __due_2020-12-31__ or just __due_now__
+    __connect_parents__ can only be set on the top level and means that the parents (second level) will be connected chronologicaly
     `,
       },
       { role: "user", content: "Current WBS: " + currentContent },
